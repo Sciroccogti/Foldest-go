06e155b7b77cadd6fca3028bbe835f13cd89ab05
Sciroccogti <scirocco_gti@yeah.net>
2021-01-01 21:54:17 +0800

    change to ui

--- foldest.go
+++ foldest.go
@@ -3,27 +3,54 @@ package main
 import (
 	"fmt"
 	"foldest-go/utils"
+	"time"
+
+	"github.com/andlabs/ui"
+	_ "github.com/andlabs/ui/winmanifest"
 )
 
 func main() {
-	conf := utils.ReadConf()
-	fmt.Println("Press enter to start...")
-	fmt.Scanln()
+	err := ui.Main(func() {
+		startBtn := ui.NewButton("Start")
+		startBtn.OnClicked(func(*ui.Button) {
+			startTime := time.Now().UnixNano()
 
-	rules := utils.ReadRules()
-	if rules == nil {
-		fmt.Println("Skipping classify...")
-	} else {
-		utils.DoClassify(rules, conf.Targetdir, conf.Verbose)
+			conf := utils.ReadConf()
+
+			rules := utils.ReadRules()
+			if rules == nil {
+				fmt.Println("Skipping classify...")
+			} else {
+				utils.DoClassify(rules, conf.Targetdir, conf.Verbose)
+			}
+
+			if conf.Tmpbin.Enable {
+				fmt.Println("Performing tmpbin...")
+				utils.Manage(conf)
+			} else {
+				fmt.Println("tmpbin is disabled, skipping...")
+			}
+
+			endTime := time.Now().UnixNano()
+			seconds := float64((endTime - startTime) / 1e9)
+			fmt.Printf("Finished in %.2f sec.\n", seconds)
+		})
+
+		outputLabel := ui.NewLabel("")
+
+		canvas := ui.NewVerticalBox()
+		canvas.Append(startBtn, false)
+		canvas.Append(outputLabel, true)
+		window := ui.NewWindow("Foldest", 300, 300, false)
+		window.SetChild(canvas)
+		window.SetMargined(true)
+		window.OnClosing(func(*ui.Window) bool {
+			ui.Quit()
+			return true
+		})
+		window.Show()
+	})
+	if err != nil {
+		panic(err)
 	}
-
-	if conf.Tmpbin.Enable {
-		fmt.Println("Performing tmpbin...")
-		utils.Manage(conf)
-	} else {
-		fmt.Println("tmpbin is disabled, skipping...")
-	}
-
-	fmt.Println("Press enter to exit...")
-	fmt.Scanln()
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module foldest-go
 
 go 1.13
 
-require gopkg.in/yaml.v2 v2.2.8
+require (
+	github.com/andlabs/ui v0.0.0-20200610043537-70a69d6ae31e
+	gopkg.in/yaml.v2 v2.2.8
+)
